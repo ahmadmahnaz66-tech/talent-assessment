e039b5599f5eee1da6da8d5dd4956214afa19ab9
ahmadmahnaz66-tech <ahmadmahnaz66@gmail.com>
2026-09-07 23:05:33 +0330

    Update admin.js

--- admin.js
+++ admin.js
@@ -383,3 +383,18 @@ async function deleteSelectedSkill() {
   });
   if (res.ok) await loadInitialMetadata();
 }
+
+function downloadSampleCSV() {
+  const csvContent = "\uFEFFid,student_name,grade,classroom,parent_phone\n" +
+                     "101,نام و نام خانوادگی نمونه,چهارم,۴/۱,09123456789\n";
+
+  const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.setAttribute('href', url);
+  link.setAttribute('download', 'نمونه_دانش_آموزان.csv');
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
